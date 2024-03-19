1f447ba935222feec117de24e5746fa511ca2400
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-03-20 00:01:50 +0530

    Update args.go

--- exercises/003-args/args.go
+++ exercises/003-args/args.go
@@ -18,5 +18,7 @@ import "fmt"
 func main() {
 	// Knowing how to separate arguments in function calls fix
 	// the following line so that it prints "go 2 true"
-	fmt.Println("go" 2 true)
+	var number int = 2
+	var isLoggedIn bool = true
+	fmt.Println("go", number, isLoggedIn)
 }
